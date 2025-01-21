2829d17592a8c6d7eeab849fe746c8281e1a475a
AzrilYusuf <muhammadazrilyusuf.w3rk@gmail.com>
2025-01-22 01:54:58 +0700

    feat(sign-in-validation): Enhance signin route with validation middleware and update login user validation logic.

--- dist/src/validators/auth.validator.js
+++ dist/src/validators/auth.validator.js
@@ -36,7 +36,28 @@ class AuthValidator {
                 .matches(/[A-Z]/)
                 .withMessage('Password must contain at least one uppercase letter')
                 .matches(/[a-z]/)
-                .withMessage('Password must contain at least one lowercase letter')
+                .withMessage('Password must contain at least one lowercase letter'),
+        ];
+    }
+    static loginUserValidator() {
+        return [
+            (0, express_validator_1.body)('email')
+                .trim()
+                .notEmpty()
+                .withMessage('Email is required')
+                .isEmail()
+                .withMessage('Email is not valid'),
+            (0, express_validator_1.body)('password')
+                .notEmpty()
+                .withMessage('Password is required')
+                .isLength({ min: 8 })
+                .withMessage('Password must be at least 8 characters long')
+                .matches(/\d/)
+                .withMessage('Password must contain at least one number')
+                .matches(/[A-Z]/)
+                .withMessage('Password must contain at least one uppercase letter')
+                .matches(/[a-z]/)
+                .withMessage('Password must contain at least one lowercase letter'),
         ];
     }
 }
