260b7a8f04ef9644057cc8017fb6abeb1f99490d
AzrilYusuf <muhammadazrilyusuf.w3rk@gmail.com>
2025-01-27 02:13:20 +0700

    refactor(file-uploader, users-controller, users.model): Rename uploader middleware and update related routes; add comments in user-picture functionalities for readability and add typings.

--- dist/src/routes/users.router.js
+++ dist/src/routes/users.router.js
@@ -9,14 +9,14 @@ const authMiddleware_1 = __importDefault(require("../middlewares/authMiddleware"
 const authAdminMiddleware_1 = __importDefault(require("../middlewares/authAdminMiddleware"));
 const validator_1 = __importDefault(require("../validators/validator"));
 const validationMiddleware_1 = __importDefault(require("../middlewares/validationMiddleware"));
-const uploaderMiddleware_1 = __importDefault(require("../middlewares/uploaderMiddleware"));
+const fileUploaderMiddleware_1 = __importDefault(require("../middlewares/fileUploaderMiddleware"));
 const usersRouter = express_1.default.Router();
 usersRouter.get('/all-users', authMiddleware_1.default, authAdminMiddleware_1.default, users_controller_1.default.getAllUsers);
 usersRouter.get('/', authMiddleware_1.default, users_controller_1.default.getUserById);
 usersRouter.put('/', authMiddleware_1.default, ...validator_1.default.updateUserValidator(), validationMiddleware_1.default, users_controller_1.default.updateUser);
-usersRouter.post('/picture', authMiddleware_1.default, uploaderMiddleware_1.default.single('file'), users_controller_1.default.uploadUserPicture);
+usersRouter.post('/picture', authMiddleware_1.default, fileUploaderMiddleware_1.default.single('file'), users_controller_1.default.uploadUserPicture);
 usersRouter.get('/picture', authMiddleware_1.default, users_controller_1.default.getUrlPicture);
-usersRouter.patch('/picture', authMiddleware_1.default, uploaderMiddleware_1.default.single('file'), users_controller_1.default.updateUserPicture);
+usersRouter.patch('/picture', authMiddleware_1.default, fileUploaderMiddleware_1.default.single('file'), users_controller_1.default.updateUserPicture);
 usersRouter.delete('/picture', authMiddleware_1.default, users_controller_1.default.deleteUserPicture);
 usersRouter.delete('/:id', authMiddleware_1.default, authAdminMiddleware_1.default, users_controller_1.default.deleteUser);
 exports.default = usersRouter;
